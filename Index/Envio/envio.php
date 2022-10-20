a0b6a393531baf75c0233e44c63e1260741c5746
Luciano Piehl <luchopinamar123@gmail.com>
2022-10-20 18:37:05 -0300

    No RWD

---
+++ Index/Envio/envio.php
@@ -0,0 +1,5 @@
+<link rel="stylesheet" type="text/css" href="../Envio/envioestilo.css">
+<p id="sentence" contenteditable="true" spellcheck="false">
+  Gracias por suscribirte a nuestro  <strong>Newsletter</strong> pronto enviaremos informacion a tu email, <strong> Estate atento! </strong>
+</p>
+
